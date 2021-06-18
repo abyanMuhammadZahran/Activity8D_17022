1b7d66ccd0eff32bae83e4ab6ea97cec154dd05c
AbyanMuhammadZahran <zahraan.abyan@gmail.com>
2021-06-18 21:29:58 +0700

    penambahan comment

--- app/src/main/java/com/example/activity8d_17022/Cari.java
+++ app/src/main/java/com/example/activity8d_17022/Cari.java
@@ -29,6 +29,7 @@ public class Cari extends AppCompatActivity {
         batal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //finish untuk keluar dari activity atau selesai gitulah
                 finish();
             }
         });
